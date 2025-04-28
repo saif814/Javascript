7aa707b2e4ebf6582f9e93e8002994aeb785ff71
Saif814 <saifsidd710@gmail.com>
2025-04-28 12:59:57 +0530

    Added notes for arithmetic operators

---
+++ operators/arithmetic.js
@@ -0,0 +1,21 @@
+/*
+
+        Operator	 Description
+---------------------------------------------------------
+            +	        Addition
+            -	        Subtraction
+            *	        Multiplication
+            **	        Exponentiation 
+            /	        Division
+            %	        Modulus (Division Remainder)
+            ++	        Increment
+            --	        Decrement
+
+
+*/
+
+//usage example
+let num1 = 5
+let num2 = 2
+let Remainder = num1 % num2
+console.log(Remainder) //output 1
