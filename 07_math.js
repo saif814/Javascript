db0ffa45009a02bd1e6f2a01ec790e85e94a3028
Avast45 <vilrn5677@gmail.com>
2025-05-01 23:41:32 +0530

    math properties

---
+++ 07_math.js
@@ -0,0 +1,41 @@
+// Math Properties (Constants)
+
+// The syntax for any Math property is : Math.property.
+
+// JavaScript provides 8 mathematical constants that can be accessed as Math properties:
+
+// Math.E        // returns Euler's number
+// Math.PI       // returns PI
+// Math.SQRT2    // returns the square root of 2
+// Math.SQRT1_2  // returns the square root of 1/2
+// Math.LN2      // returns the natural logarithm of 2
+// Math.LN10     // returns the natural logarithm of 10
+// Math.LOG2E    // returns base 2 logarithm of E
+// Math.LOG10E   // returns base 10 logarithm of E
+
+console.log(Math.LOG2E)
+
+//math methods 
+// The syntax for Math any methods is : Math.method(number)
+
+// Number to Integer
+
+// Math.round(x)	Returns x rounded to its nearest integer
+// Math.ceil(x)	Returns x rounded up to its nearest integer
+// Math.floor(x)	Returns x rounded down to its nearest integer
+// Math.trunc(x)	Returns the integer part of 
+
+// Math.trunc(x)	Returns the integer part of
+console.log(4.9)
+
+
+
+// Math.pow()
+// Math.pow(x, y) returns the value of x to the power of 
+
+console.log(Math.pow(3 , 4))
+
+// Math.abs()
+// Math.abs(x) returns the absolute (positive) value of x
+
+console.log(Math.abs(-9.1))
